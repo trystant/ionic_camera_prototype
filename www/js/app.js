cb60629a7bc8ce63542b477372cfe35b04b249a6
Mark Nyon <mark@grandkru.com>
2015-09-22 19:30:08 -0400

    Added camera functionality using example app
    This adds basic camera usage to the functionality of the Ionic app.
    
    - www/index.html - Adds an ion-content tag which references the
      example controller that is used to activate the camera.
    
    - www/js/app.js - Adds an ExampleController that takes a picture
      using the Cordova camera plugin
    
    - www/js/ng-cordova.js - The ngCordova wrapper that wraps Cordova
      plugins in Angular services for ease of use.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,28 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+exampleApp.controller("ExampleController", function($scope, $cordovaCamera) {
+
+    $scope.takePicture = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
+
+});
